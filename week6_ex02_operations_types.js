a5f0602ad86882fd738ff6cb9c35f8bab575856c
Morgen Swartz <morgenswartz619@gmail.com>
2026-05-18 13:24:14 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,14 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 34;
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 7;
+// TODO: Convert number1 to a string and store it in a new variable
+newNumber1 = String(number1);
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2;
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2;
+// TODO: Print all results to the console
+console.log(newNumber1);
+console.log(sum);
+console.log(product);
