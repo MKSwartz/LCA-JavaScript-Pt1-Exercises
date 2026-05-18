e0198562e3eeb8bc81826c0d098e1b3169d5ec73
Morgen Swartz <morgenswartz619@gmail.com>
2026-05-18 14:30:59 +0200

    Add Exercise 03 - Array Basics

---
+++ week6_ex03_arrays.js
@@ -0,0 +1,13 @@
+// TODO: Create an empty array called colors
+const colors = [];
+// TODO: Add three colors to the array using push()
+colors.push("Blue", "Green", "Purple");
+// TODO: Create another array called numbers with five numbers
+const numbers = [3, 4, 1, 5, 2];
+// TODO: Remove the last color from the colors array
+colors.pop();
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("Peach");
+// TODO: Print the length of both arrays
+console.log("Colors length:", colors.length);
+console.log("Numbers length:", numbers.length);
